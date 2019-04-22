1530fec1608ee42cea149c496d1b12425aafdff2
sararin <32796093+sararin@users.noreply.github.com>
2019-04-22 18:28:35 +0200

    Update avatarme.go

--- avatarme.go
+++ avatarme.go
@@ -7,9 +7,14 @@ import (
 	"os"
 )
 
-func Draw(b []byte) io.WriterCloser {
-	a, _ := uniqhash.Encrypt(b)
-	encoder := base64.NewEncoder(base64.StdEncoding, os.Stdout)
-	encoder.Write(drawimg.FromHash(a))
-	encoder.Close()
+func Draw(b []byte) (string, error) {
+	a, err := uniqhash.Encrypt(b)
+	if err != nil {
+		return "", err
+	}
+	result, err := drawimg.FromHash(a) //should return []byte png.Encode(&buffer, imageData) should do the trick
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(result), nil
 }
